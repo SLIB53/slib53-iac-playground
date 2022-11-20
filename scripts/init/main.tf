5d2e0bc483429f44316f1758275f26b7f6a3cfed
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2022-11-20 07:03:40 +0000

    Add init script

---
+++ scripts/init/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = "SLIB53"
+
+      Environment = "Root"
+      Owner       = "Root"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "slib53_iac_tfstate" {
+  bucket = "slib53-iac-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "slib53_iac_tfstate" {
+  bucket = aws_s3_bucket.slib53_iac_tfstate.id
+
+  versioning_configuration {
+    status     = "Enabled"
+  }
+}
